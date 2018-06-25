c2c8522c35c5dd9e1f44387b66b5062ecae340e7
stefano <stefano@debian>
2018-06-25 22:02:50 +0100

    restored

--- cases/admin.py
+++ cases/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-# from cases.models import Platforms, Reports
-# 
-# admin.site.register(Platforms)
-# admin.site.register(Reports)
+from cases.models import Platforms, Reports
+ 
+admin.site.register(Platforms)
+admin.site.register(Reports)
--- cases/models.py
+++ cases/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class Platforms(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=50)
-#     api_key = models.CharField(max_length=100)
-#     
-#     
-# class Reports(models.Model):
-#     report_id = models.AutoField(primary_key=True)
-#     platform = models.ForeignKey(Platforms, on_delete=models.CASCADE)
-#     report_name = models.CharField(max_length=50)
-#     
-# 
-# # Create your models here.
-# class Cases(models.Model):
-#     QUERY_STATUS = (
-#         ('c', 'completed'),
-#         ('f', 'failed'),
-#         ('r', 'running'),
-#     )
-#     query_id = models.AutoField(primary_key=True)
-#     platform = models.ForeignKey(Platforms, on_delete=models.DO_NOTHING)
-#     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-#     creation_date = models.DateTimeField(auto_now_add=True)
-#     report_type = models.ForeignKey(Reports, on_delete=models.DO_NOTHING)
-#     query_title = models.CharField(max_length=100)
-#     status = models.CharField(max_length=1, choices=QUERY_STATUS)
+class Platforms(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    api_key = models.CharField(max_length=100)
+     
+     
+class Reports(models.Model):
+    report_id = models.AutoField(primary_key=True)
+    platform = models.ForeignKey(Platforms, on_delete=models.CASCADE)
+    report_name = models.CharField(max_length=50)
+     
+ 
+# Create your models here.
+class Cases(models.Model):
+    QUERY_STATUS = (
+        ('c', 'completed'),
+        ('f', 'failed'),
+        ('r', 'running'),
+    )
+    query_id = models.AutoField(primary_key=True)
+    platform = models.ForeignKey(Platforms, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    report_type = models.ForeignKey(Reports, on_delete=models.DO_NOTHING)
+    query_title = models.CharField(max_length=100)
+    status = models.CharField(max_length=1, choices=QUERY_STATUS)
     
