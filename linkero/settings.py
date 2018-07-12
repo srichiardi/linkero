13e292755d4aa2419d2d83e2ea0c152858ea7c0f
stefano <stefano@debian>
2018-07-12 21:57:31 +0100

    adding celery settings

--- linkero/settings.py
+++ linkero/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import djcelery
+
+## Celery configuration
+djcelery.setup_loader()
+BROKER_URL = 'django://'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cases',
     'django.contrib.admin',
+    'djcelery',
+    'kombu.transport.django',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +145,5 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
+
+
