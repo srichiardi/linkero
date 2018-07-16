71be8aa05e3dd5a04a0028841a19fcf26f97dd03
srichiardi <srichiardi@l-dub-13002487.corp.ebay.com>
2018-07-16 14:28:36 +0100

    adding celery with redis

--- linkero/settings.py
+++ linkero/settings.py
@@ -11,12 +11,16 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import djcelery
 import mongoengine
 
 ## Celery configuration
-djcelery.setup_loader()
-BROKER_URL = 'django://'
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Dublin'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
