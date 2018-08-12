cc18340e5c9d0f8a1cacd70e300ee07231fd0f77
stefano <stefano@debian>
2018-08-12 21:55:18 +0100

    adj

--- cases/views.py
+++ cases/views.py
@@ -185,7 +185,7 @@ class FileDownload(LoginRequiredMixin, View):
 #         writer = csv.DictWriter(csv_file, fieldnames=headers)
 #         writer.writeheader()
 #         writer.writerows(df.to_dict('records'))
-        response = HttpResponse(csv_file, content_type='text/csv')
+        response = HttpResponse(csv_file, content_type='application/vnd.ms-excel')
         response['Content-Disposition'] = 'attachment; filename="linkero_file.csv"'
         #response['Content-Length'] = csv_file.tell()
         
