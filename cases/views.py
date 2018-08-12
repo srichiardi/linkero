de20a0e58149b46347ab65bc54ec0eca409835b7
stefano <stefano@debian>
2018-08-12 21:42:48 +0100

    adj

--- cases/views.py
+++ cases/views.py
@@ -181,7 +181,7 @@ class FileDownload(LoginRequiredMixin, View):
         # return the file
         csv_file = BytesIO()
         writer = csv.DictWriter(csv_file, fieldnames=headers)
-        writer.writeheader()
+        #writer.writeheader()
         writer.writerows(df.to_dict('records'))
         response = HttpResponse(csv_file, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="linkero_file.csv"'
