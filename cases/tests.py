b05e4159cc686f6cd619accff34177a88938341e
stefano <stefano@debian>
2018-12-11 20:56:49 +0000

    first test

--- cases/tests.py
+++ cases/tests.py
@@ -8,17 +8,18 @@ class FindItemsTest(TestCase):
     # test if the method accepts an eBay site not correctly formatted
     def test_bad_ebay_site(self):
         wrong_site = 'PK'
-        self.assertRaises(EbayBadsite)
+        with self.assertRaises(EbayBadSite):
+            self.ea.find_items(ebay_site=wrong_site, keywords="baby oil")
     
-    # test a page higer than the 100th limit allowed
-    def test_wrong_page(self):
-        wrong_page = 203
-        self.assertRaises(EbayPageOutOfLimit)
-    
-    # test input without seller id nor keywords
-    def test_missing_input(self):
-        self.assertRaises(EbayMissingInput)
-        
-    # test if loading more than 20 items
-    def test_too_many_items(self):
-        self.assertRaises(EbayTooManyItems)
+#     # test a page higer than the 100th limit allowed
+#     def test_wrong_page(self):
+#         wrong_page = 203
+#         self.assertRaises(EbayPageOutOfLimit)
+#     
+#     # test input without seller id nor keywords
+#     def test_missing_input(self):
+#         self.assertRaises(EbayMissingInput)
+#         
+#     # test if loading more than 20 items
+#     def test_too_many_items(self):
+#         self.assertRaises(EbayTooManyItems)
